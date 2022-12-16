db2f012c5aaca00780a02bc23fb0f4c9ae275a1a
henry <henry.981@hotmail.it>
2022-12-16 19:12:59 +0100

    fix terminate session button

--- src/main/java/it/unicas/bms_project/MainApp.java
+++ src/main/java/it/unicas/bms_project/MainApp.java
@@ -104,7 +104,7 @@ public class MainApp extends Application {
 
         // Set the application icon.
         this.primaryStage.getIcons().add(new Image("file:src/main/resources/images/battery.png"));
-        this.primaryStage.setWidth(1120);
+        this.primaryStage.setWidth(1150);
         this.primaryStage.setHeight(700);
 
         initRootLayout();
